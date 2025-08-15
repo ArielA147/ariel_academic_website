ca6afb94d742df7773ebdc4a402207333b11701b
Ariel Fenster <arielfenster1610@gmail.com>
2025-08-15 22:08:06 +0300

    consolidated publications page to a separate folder

--- publications/publications.js
+++ publications/publications.js
@@ -1,11 +1,11 @@
 // imports
-import { PageRender, retrivedData } from '../../js/pageRender.js';
-import { PublicationCard } from '../../js/components/publicationCard.js';
-import { addCollapseFunction } from '../../js/descriptionSlicer.js';
-import { Icons } from '../../js/components/icons.js';
+import { PageRender, retrivedData } from '../js/pageRender.js';
+import { PublicationCard } from '../js/components/publicationCard.js';
+import { addCollapseFunction } from '../js/descriptionSlicer.js';
+import { Icons } from '../js/components/icons.js';
 
 // Data file paths
-let PUBLICATIONS_JSON = 'data/jsons/academic-publications.json';
+let PUBLICATIONS_JSON = '/data/jsons/academic-publications.json';
 
 // consts
 const default_sorter = 'year';
