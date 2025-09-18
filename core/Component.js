b796e45c3fb9a14c8466b26bc38e2899b02ce596
Ariel Fenster <arielfenster1610@gmail.com>
2025-09-19 01:39:12 +0300

    removed commented code

--- core/Component.js
+++ core/Component.js
@@ -8,20 +8,6 @@ export class Component {
 		this.#name = name;
 	}
 
-	// TODO: remove this function
-	// /**
-	//  * @param {string} path
-	//  */
-	// async loadTemplate(path) {
-	// 	const template = await DataLoader.loadComponent(path);
-
-	// 	const parser = new DOMParser();
-	// 	const doc = parser.parseFromString(template, 'text/html');
-	// 	const templateElement = doc.querySelector('template');
-
-	// 	return templateElement.content.firstElementChild;
-	// }
-
 	/**
 	 * @returns {DocumentFragment}
 	 */
@@ -33,9 +19,6 @@ export class Component {
 		return template.content.cloneNode(true);
 	}
 
-	// TODO: should this component add the component to the DOM/input container?
-	// should it only created it and return it?
-	// if so, should the rename to something else?
 	render() {
 		throw new Error('Render method must be implemented in subclass');
 	}
